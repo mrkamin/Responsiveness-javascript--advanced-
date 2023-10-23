ed6b9e2dca92573f8020f959d35cbcd27dc23a39
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-10-23 17:31:32 +0500

    make page seven left side destop version responsiv

--- src/components/PageSeven.js
+++ src/components/PageSeven.js
@@ -16,7 +16,7 @@ const PageSeven = () => {
 
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
-      gsap.from('.image-container', {
+      gsap.from('sect-seven-left-side-item-three-item-one', {
         duration: 2,
         y: '-100%',
         ease: 'bounce',
