1328eb09978b1ade7d9f0312395ea43317bafc96
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-10-25 11:28:37 +0500

    Make code clean and dry

--- src/components/Desktopveiw.js
+++ src/components/Desktopveiw.js
@@ -1,11 +1,9 @@
 import React from 'react';
-// import Svg from './Svg';
 import ScrollEvent from './ScrollEvent';
 
 const Desktopveiw = () => (
   <>
     <div className="desktopveiw-container position-relative ">
-      {/* <Svg /> */}
       <ScrollEvent />
     </div>
   </>
